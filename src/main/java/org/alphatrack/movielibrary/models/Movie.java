225dd065dfd2e1823f207f3be7cfcbeb8441c364
Svetoslav <niftoriuss@gmail.com>
2026-07-22 10:38:59 +0200

    feat: implement Movie model

--- src/main/java/org/alphatrack/movielibrary/models/Movie.java
+++ src/main/java/org/alphatrack/movielibrary/models/Movie.java
@@ -1,4 +1,32 @@
 package org.alphatrack.movielibrary.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "movies")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String director;
+
+    @Column(nullable = false)
+    private Integer releaseYear;
+
+    @Column
+    private Double rating;
+
 }
