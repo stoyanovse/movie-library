a8cfa872090afa6d5c74a74ada2cf9f42fb5a032
Svetoslav <niftoriuss@gmail.com>
2026-09-05 20:43:53 +0200

    add redirect controller

---
+++ src/main/java/org/alphatrack/movielibrary/controllers/rest/RootRedirectController.java
@@ -0,0 +1,13 @@
+package org.alphatrack.movielibrary.controllers.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootRedirectController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
