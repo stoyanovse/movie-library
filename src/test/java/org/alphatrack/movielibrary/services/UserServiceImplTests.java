4253b077f1b3facbcad41d0f0a323bd8385805ff
Svetoslav <niftoriuss@gmail.com>
2026-09-05 19:50:24 +0200

    clean unused imports

--- src/test/java/org/alphatrack/movielibrary/services/UserServiceImplTests.java
+++ src/test/java/org/alphatrack/movielibrary/services/UserServiceImplTests.java
@@ -19,7 +19,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 
 import java.util.List;
